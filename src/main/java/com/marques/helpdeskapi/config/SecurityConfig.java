83e1750b8fd4ddf0a9e441630b9765c53a24c4d1
jjoao-marques <joaomarques.jm991@gmail.com>
2021-11-27 18:39:08 -0300

    Ajuste CORS

--- src/main/java/com/marques/helpdeskapi/config/SecurityConfig.java
+++ src/main/java/com/marques/helpdeskapi/config/SecurityConfig.java
@@ -62,9 +62,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-		configuration.setAllowedMethods(Arrays.asList("POST", "GET","PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("*", configuration);
+		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
 	
